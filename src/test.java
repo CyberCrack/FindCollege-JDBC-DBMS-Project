31f2e85bc7de7b33389db8f131f44b8172fa6d4f
Cyber Crack <mayankanuragi2013@gmail.com>
2019-11-26 19:49:35 +0530

    Initial Commit.

--- src/test.java
+++ src/test.java
@@ -10,6 +10,8 @@ class test
         System.out.println(v.isEmpty());
         System.out.println("965852639".matches("^[0-9]*$"));
         System.out.println("@fgd.com".matches("^[A-Za-z0-9+_.-]+@(.+)$"));
+        String a = "dwasd" + 5;
+        System.out.println(a);
 
 
     }
